afb2547c5852af4ce3d111d142a9b2d97e8d3447
Tamiris Shigaki <shigakiendo@gmail.com>
2022-06-03 23:22:29 -0300

    :sparkles: feat: Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,20 @@
+SELECT 
+    nome AS nome_musica,
+    CASE
+        WHEN nome LIKE '%Streets' THEN REPLACE(nome, 'Streets', 'Code Review')
+        WHEN nome LIKE '%Her Own' THEN REPLACE(nome, 'Her Own', 'Trybe')
+        WHEN nome LIKE '%Inner Fire' THEN REPLACE(nome, 'Inner Fire', 'Project')
+        WHEN nome LIKE '%Silly' THEN REPLACE(nome, 'Silly', 'Nice')
+        WHEN nome LIKE '%Circus' THEN REPLACE(nome, 'Circus', 'Pull Request')
+        ELSE NULL
+    END AS novo_nome
+FROM
+    SpotifyClone.cancoes
+WHERE
+    nome LIKE '%Streets'
+        OR nome LIKE '%Her Own'
+        OR nome LIKE '%Inner Fire'
+        OR nome LIKE '%Silly'
+        OR nome LIKE '%Circus'
+ORDER BY nome;
+
